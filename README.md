aa351f936d2eea16da8f41a7353017b30db63b88
DT786 <dtahir2018@icloud.com>
2026-01-15 15:06:36 +0000

    README Updated

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # learning-c
-My journey learning the C programming language
+My journey learning the C programming language.
+This repository contains the code while learning the C programming language.
+
+**Resources**
+* C Programming: A Modern Approach - K. N. King
---
+++ pun.c
@@ -0,0 +1,16 @@
+/* Name: pun.c
+ * Purpose: Prints a bad pun.
+ * Author: K. N. King
+ */
+
+#include <stdio.h>
+
+int main(void) // Beginning of main program
+{
+ int height, length, width, volume;
+ float profit, loss;
+ 
+ printf("To C, or not to C: that is the question.\n");
+
+ return 0;
+}
