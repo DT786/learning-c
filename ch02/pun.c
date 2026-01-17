e67570bb66ed771bbe075158daeb40365589bc11
DT786 <dtahir2018@icloud.com>
2026-01-17 20:16:37 +0000

    Completed section 2.4: Variables And Assignment

--- ch02/pun.c
+++ ch02/pun.c
@@ -7,9 +7,18 @@
 
 int main(void) // Beginning of main program
 {
- int height, length, width, volume;
+ int height = 8, length = 12;
+ int width, volume;
  float profit, loss;
+
+ width = 10;
+ volume = height * length * width;
+ profit = 2150.48f;
  
+ printf("Height: %d\n", height);
+ printf("Profit: $%.2f\n", profit); // The .2 in %.2f makes sure there are only 2 digits after the decimal point.
+ printf("Height: %d Length: %d\n", height, length);
+
  printf("To C, or not to C: that is the question.\n");
 
  return 0;
