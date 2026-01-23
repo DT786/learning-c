c1fe435b746e7078a6e160e06655f49a01b9ba5f
DT786 <dtahir2018@icloud.com>
2026-01-23 14:56:43 +0000

    Completed chapter 02 exercises

---
+++ ch02/exercises/ex04/program04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c, d, e;
+	float f, g, h, i, j;
+
+	printf("int variables:\n %d\n %d\n %d\n %d\n %d\n", a, b, c, d, e);
+	printf("float variables:\n %f\n %f\n %f\n %f\n %f\n", f, g, h, i, j);
+
+	return 0;
+}
